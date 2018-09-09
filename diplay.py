11b3d5852757c5dd592952ca8e689083c9f6dde1
obhurr <43034408+obhurr@users.noreply.github.com>
2018-09-09 20:42:05 +0200

    disp

--- diplay.py
+++ diplay.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from Adafruit_LED_Backpack import SevenSegment
 from time import time, sleep, strftime, gmtime
+import threading
 
 global segment
 
@@ -9,13 +10,14 @@ segment = SevenSegment.SevenSegment(address=0x70)
 segment.begin()
 segment.set_brightness(0)
 
+
+
 def Slot_disp():
     ## Continually update the time on a 4 char, 7-segment display
     global segment
-
-    x = 0
-
-    while True:
+    x = 9
+    y = 0
+    while x >= 0:
         segment.clear()
         segment.set_digit(0, x)     # Tens
         segment.set_digit(1, x)       # Ones
@@ -24,10 +26,8 @@ def Slot_disp():
         segment.set_colon(False)
         segment.write_display()
 
-        if x == 9:
-            x = 0
-        else:
-            x = x + 1
-        sleep(0.05)
+        x = x - 1
+        y = y + 0.05
+        sleep(y)
 
 Slot_disp()
