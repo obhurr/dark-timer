8ae3e1a91b15194136676f7b66815bb08d48fbf7
obhurr <43034408+obhurr@users.noreply.github.com>
2018-09-09 21:57:00 +0200

    deb

--- timer.py
+++ timer.py
@@ -39,7 +39,7 @@ def PwrOff():
     diff = 0
     Single = True
     start_time = time()
-    while Vensre.is_active and (diff < hold_time) :
+    while Vensre.is_active and Single :
         if Enc_B.is_pressed or Enc_A.is_pressed:
             Single = False
         else:
